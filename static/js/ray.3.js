b7ff999731539d8a7211d6897f721290d76292d6
bochen.li <bochen.li@xtalpi.com>
2023-11-20 15:47:59 +0800

    update server_launcher

--- static/js/ray.3.js
+++ static/js/ray.3.js
@@ -4,18 +4,22 @@ const region = document.getElementById('region');
 const config = document.getElementById('config');
 const textout = document.getElementById('text-out');
 const tableout = document.getElementById('table-out');
+var isDebug = (location.hostname === 'localhost');
 
-function add_row(instanceID, publicIP) {
+function add_row(instanceID, state, publicIP) {
   let newRow = tableout.querySelector("tbody").insertRow(-1);
   let c0 = newRow.insertCell(0);
   let c1 = newRow.insertCell(1);
   let c2 = newRow.insertCell(2);
   let btn = document.createElement("button");
-  c0.innerText = instanceID;
+  c0.innerText = state;
   c1.innerText = publicIP;
-  btn.innerText = "STOP";
-  c2.appendChild(btn);
-  btn.addEventListener("click", () => {stopService(instanceID, publicIP)});
+  if (!(publicIP === undefined)) {
+    btn.innerText = "Terminate";
+    btn.setAttribute("data", instanceID);
+    c2.appendChild(btn);
+    btn.addEventListener("click", () => {stopService(instanceID);queryService();});
+  }
 }
 
 function echo_time() {
@@ -58,12 +62,14 @@ function startService() {
   });
   const params = JSON.parse(config.value);
   textout.innerText = 'Starting service';
+  echo_time();
 
   ec2.runInstances(params, (err, data) => {
     if (err) {
       textout.innerText = err + '\n' + err.stack;
     } else {
-      textout.innerText = JSON.stringify(data);
+      textout.innerText = JSON.stringify(data, null, 2);
+      queryService();
     }
   });
   echo_time();
@@ -78,8 +84,8 @@ function queryService() {
   const params = {
     Filters: [
       {
-        Name: 'instance-state-name',
-        Values: ['running']
+        Name: 'tag:Name',
+        Values: ['MyXrayServer*']
       }
     ]
   };
@@ -91,9 +97,12 @@ function queryService() {
     } else {
       data.Reservations.forEach(function(reservation) {
         reservation.Instances.forEach(function(instance) {
-          add_row(instance.InstanceId, instance.PublicIpAddress);
+          add_row(instance.InstanceId, instance.State.Name, instance.PublicIpAddress);
         });
       });
+      if (isDebug) {
+        console.log(data.Reservations);
+      }
     }
   });
   echo_time();
@@ -122,3 +131,4 @@ function stopService(instanceID) {
 
 echo_time();
 loadCredential();
+queryService();
